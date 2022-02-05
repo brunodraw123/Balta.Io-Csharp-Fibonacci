1274de38e6a1d5d1ffd7915dc37a16e2ae145558
Bruno <brunodraw123@gmail.com>
2022-02-04 23:24:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _Fibonacci
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Quantos números da sequência de fibonacci deseja ver?");
+            int tamanhoArray = int.Parse(Console.ReadLine());// quantidade de numeros que sera mostrado
+            if (tamanhoArray >= 2)
+            {
+                SequenciaFIbocciExtensa(tamanhoArray);
+            }
+            else
+            {
+                SequenciaFIbocciCurta(tamanhoArray);
+            }
+
+        }
+        public static void SequenciaFIbocciCurta(int tamanhoArray)
+        {
+            if (tamanhoArray == 0) { Console.WriteLine("Inválido"); }//caso numero seja 0
+            else
+            { Console.WriteLine("0"); }//caso numero seja 1
+        }
+        public static void SequenciaFIbocciExtensa(int tamanhoArray)
+        {
+            int arrayUltimoIndice = tamanhoArray - 1;// numero do ultimo indice que tera o array.
+            int[] fibonacci = new int[tamanhoArray];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i <= arrayUltimoIndice; i++)//calcula;
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+
+            for (int i = 0; i < fibonacci.Length; i++)//printa;
+            {
+                Console.Write(fibonacci[i] + " ");
+            }
+        }
+
+
+    }
+}
